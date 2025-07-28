11f8ab1e3bf80d1774a8095d2a55af085cc028b7
Lenka Ertl <drahozalovalenka@seznam.cz>
2025-07-29 01:59:52 +0200

    Kalkulačka v JavaScriptu pro HW4

--- javascript-learning/DÚ/kalkulacka.js
+++ javascript-learning/DÚ/kalkulacka.js
@@ -2,3 +2,10 @@ console.log("+:", 565 + 584);
 console.log("-:", 474 - 520);
 console.log("*:", 5822 * 65);
 console.log("**:", 632 / 6);
+
+/* $ node 'c:/Users/Windows/Documents/GITProjects/cypress_summer25_javascript/javascript-learning/DÚ/kalkulacka.js'
++: 1149
+-: -46
+*: 378430
+**: 105.33333333333333
+*/
